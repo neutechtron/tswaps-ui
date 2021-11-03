a37401fb059ed0dc48f879c97202588626172b9b
JPBeukes <jpbeukes27@gmail.com>
2021-11-04 01:28:53 +0200

    Added input amount component

--- src/components/FromCard.vue
+++ src/components/FromCard.vue
@@ -2,6 +2,27 @@
   <q-card class="inputCard">
     <div class="row justify-between">
       <div>From</div>
+      <div v-if="isAuthenticated">Balance: 0 TLOS</div>
+    </div>
+    <div class="row flex-wrap">
+      <input-amount class="col-xs-12 col-sm-6" />
+      <div class="col-3">Coin</div>
+      <div class="col-3">Network</div>
     </div>
   </q-card>
 </template>
+<script>
+import { mapGetters, mapActions } from "vuex";
+import inputAmount from "src/components/InputAmount";
+export default {
+  components: { inputAmount },
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapGetters("account", ["isAuthenticated", "accountName"])
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/InputAmount.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <input
+      inputmode="decimal"
+      title="Amount"
+      autocomplete="off"
+      autocorrect="off"
+      type="text"
+      pattern="^[0-9]*[.,]?[0-9]*$"
+      placeholder="0.0"
+      minlength="1"
+      maxlength="79"
+      spellcheck="false"
+    />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+body.body--light {
+  input {
+    border-color: rgba($dark, 0.8);
+    color: $dark;
+  }
+}
+
+body.body--dark {
+  input {
+    border-color: rgba($white, 0.8);
+    color: $white;
+  }
+}
+
+input {
+  width: 100%;
+  background: transparent;
+  border-top: none;
+  border-right: none;
+  border-left: none;
+  border-bottom-width: 1px;
+  font-size: 3rem;
+  &:focus-visible {
+    outline: none;
+  }
+}
+</style>
