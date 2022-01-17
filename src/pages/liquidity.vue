9d3cc1db515a6e1fc220cc3e08ee13f74efdf5d6
JPBeukes <jpbeukes27@gmail.com>
2022-01-17 14:37:07 +0200

    Improved liquidity page padding

--- src/pages/liquidity.vue
+++ src/pages/liquidity.vue
@@ -1,19 +1,16 @@
 <template>
-  <q-page >
+  <q-page>
     <div class="q-pa-sm">
-
       <div class="row justify-center q-my-md">
         <div class="col-12">
           <head-buttons />
         </div>
       </div>
-    
+
       <div class="row justify-center q-mt-xs">
         <div class="col-12">
-
           <q-card flat class="swapCard">
             <q-card-section class="swapCardSection">
-        
               <div class="row">
                 <div class="col-sm-12 ">
                   <from-card />
@@ -23,7 +20,9 @@
               <div class="row justify-center q-mt-xs q-mb-xs">
                 <span class="ml-2 fa-stack fa-1x">
                   <i class="fas fa-square fa-stack-2x swapSquare"></i>
-                  <i class="fas fa-arrow-down  fa-stack-1x fa-inverse swapArrow" ></i>
+                  <i
+                    class="fas fa-arrow-down  fa-stack-1x fa-inverse swapArrow"
+                  ></i>
                 </span>
               </div>
 
@@ -35,14 +34,13 @@
 
               <div class="row  q-mt-md">
                 <div class="col-12 ">
-                  <liquidity-button/>
+                  <liquidity-button />
                 </div>
               </div>
-
             </q-card-section>
           </q-card>
 
-          <div class ="row q-mx-md q-my-xl">
+          <div class="row q-mx-md q-mt-xl q-mb-md">
             <div class="col-6 relative-position ">
               <div class="text-h5 text-weight-bold">Your Liquidity</div>
             </div>
@@ -50,39 +48,33 @@
 
           <q-card flat class="swapCard">
             <q-card-section class="swapCardSection">
-        
               <div class="row  q-my-md">
                 <div class="col-12 ">
-                  <liquidity-button/>
+                  <liquidity-button />
                 </div>
               </div>
-              
             </q-card-section>
           </q-card>
-
         </div>
       </div>
-
     </div>
-
   </q-page>
 </template>
 
 <script>
 import fromCard from "src/components/liquidity/Input1.vue";
 import toCard from "src/components/liquidity/Input2.vue";
-import headButtons from "src/components/liquidity/Head.vue"
-import liquidityButton from "src/components/liquidity/liquidityButton.vue"
+import headButtons from "src/components/liquidity/Head.vue";
+import liquidityButton from "src/components/liquidity/liquidityButton.vue";
 
 export default {
   name: "Index",
-  components: { 
-    fromCard, 
-    toCard, 
+  components: {
+    fromCard,
+    toCard,
     headButtons,
     liquidityButton
-  },
-  
+  }
 };
 </script>
 
