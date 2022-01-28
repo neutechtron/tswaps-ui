eec837dc56116ec077ce4fe695847f7b6886a04e
JPBeukes <jpbeukes27@gmail.com>
2022-01-28 12:53:26 +0200

    Added url params to swap page

--- src/pages/swap.vue
+++ src/pages/swap.vue
@@ -66,10 +66,43 @@ export default {
     swapButton
   },
   computed: {
-    ...mapGetters("swap", ["getIsValidPair"])
+    ...mapGetters("swap", ["getIsValidPair"]),
+    ...mapGetters("tokens", ["getTokens"])
   },
   methods: {
-    ...mapActions("swap", ["swapToAndFrom"])
+    ...mapActions("swap", ["swapToAndFrom"]),
+    ...mapActions("pools", ["updatePools"]),
+    ...mapActions("tokens", ["updateTokens"]),
+    findToken(tokenQuery) {
+      // console.log("query:", tokenQuery);
+      let res = null;
+      if (tokenQuery) {
+        const arr = tokenQuery.split("-");
+        // console.log("arr:", arr);
+        if (arr.length > 1) {
+          const token_contract = arr[0];
+          const token_sym = arr[1];
+          res = this.getTokens.find(
+            token =>
+              token.symbol.toLowerCase().includes(token_sym.toLowerCase()) &&
+              token.contract
+                .toLowerCase()
+                .includes(token_contract.toLowerCase())
+          );
+        }
+      }
+      return res;
+    }
+  },
+  async mounted() {
+    await this.updatePools();
+    await this.updateTokens();
+    const fromToken = this.findToken(this.$route.query.fromToken);
+    // console.log(fromToken);
+    if (fromToken) this.$store.commit("swap/setFromToken", fromToken);
+    const toToken = this.findToken(this.$route.query.toToken);
+    // console.log(toToken);
+    if (toToken) this.$store.commit("swap/setToToken", toToken);
   }
 };
 </script>
