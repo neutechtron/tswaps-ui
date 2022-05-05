8ecbb7a79f443e4cabccb572bd458670d0cc45ae
Wikus <wikus.kriek.2w@gmail.com>
2022-05-05 18:04:23 +0200

    Revert "Merge branch 'bridge' of https://github.com/Yknot-BCS/tswaps-ui into bridge"
    This reverts commit 1bdbccd498081049fea1553d6ae9421972e11fc8, reversing
    changes made to 1baedd53e7257c4493a506ffc670bd2ff04ea39c.

--- src/boot/tport.js
+++ src/boot/tport.js
@@ -1,95 +1,81 @@
 const mainnetList = [
-    {
-        name: "Ethereum",
-        remoteId: 1,
-        chainName: "Ethereum Mainnet",
-        chainId: 1,
-        rpcUrls: ["https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161"],
-        nativeCurrency: {
-            name: "ETH",
-            symbol: "ETH",
-            decimals: 18,
-        },
-        blockExplorerUrls: ["https://etherscan.io"],
-        vaultContract: "",
+  {
+    name: "Ethereum",
+    remoteId: 1,
+    chainName: "Ethereum Mainnet",
+    chainId: 1,
+    rpcUrls: ["https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161"],
+    nativeCurrency: {
+      name: "ETH",
+      symbol: "ETH",
+      decimals: 18,
     },
-    {
-        name: "BSC",
-        remoteId: 2,
-        chainName: "Binance Smart Chain Mainnet",
-        chainId: 56,
-        rpcUrls: ["https://bsc-dataseed1.ninicoin.io"],
-        nativeCurrency: {
-            name: "BINANCE COIN",
-            symbol: "BNB",
-            decimals: 18,
-        },
-        blockExplorerUrls: ["https://bscscan.com"],
-        vaultContract: "0x60f20815aa24B07b3Ec7B64624B65e338aaA85e5",
+    blockExplorerUrls: ["https://etherscan.io"],
+    vaultContract: "",
+  },
+  {
+    name: "BSC",
+    remoteId: 2,
+    chainName: "Binance Smart Chain Mainnet",
+    chainId: 56,
+    rpcUrls: ["https://bsc-dataseed1.ninicoin.io"],
+    nativeCurrency: {
+      name: "BINANCE COIN",
+      symbol: "BNB",
+      decimals: 18,
     },
-    {
-        name: "Telos EVM",
-        remoteId: 3,
-        chainName: "Telos EVM Mainnet",
-        chainId: 40,
-        rpcUrls: ["https://mainnet.telos.net/evm"],
-        nativeCurrency: {
-            name: "TLOS",
-            symbol: "TLOS",
-            decimals: 18,
-        },
-        blockExplorerUrls: ["https://teloscan.io"],
-        vaultContract: "",
-    },
+    blockExplorerUrls: ["https://bscscan.com"],
+    vaultContract: "0x60f20815aa24B07b3Ec7B64624B65e338aaA85e5",
+  },
 ];
 
 const testnetList = [
-    {
-        name: "Ethereum",
-        remoteId: 1,
-        chainName: "Ropsten Test Network",
-        chainId: 3,
-        rpcUrls: ["https://ropsten.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161"],
-        nativeCurrency: {
-            name: "Ropsten ETH",
-            symbol: "ETH",
-            decimals: 18,
-        },
-        blockExplorerUrls: ["https://ropsten.etherscan.io"],
-        vaultContract: "0x02289E8B9d13C02a853620eff978821b3f0193F6",
+  {
+    name: "Ethereum",
+    remoteId: 1,
+    chainName: "Ropsten Test Network",
+    chainId: 3,
+    rpcUrls: ["https://ropsten.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161"],
+    nativeCurrency: {
+      name: "Ropsten ETH",
+      symbol: "ETH",
+      decimals: 18,
     },
-    {
-        name: "BSC",
-        remoteId: 2,
-        chainName: "Binance Smart Chain Testnet",
-        chainId: 97,
-        rpcUrls: ["https://data-seed-prebsc-1-s1.binance.org:8545"],
-        nativeCurrency: {
-            name: "BINANCE COIN",
-            symbol: "tBNB",
-            decimals: 18,
-        },
-        blockExplorerUrls: ["https://testnet.bscscan.com"],
-        vaultContract: "0x5213fcB1Cf69B029A32F9454fD29f2a500af569a",
+    blockExplorerUrls: ["https://ropsten.etherscan.io"],
+    vaultContract: "0x02289E8B9d13C02a853620eff978821b3f0193F6",
+  },
+  {
+    name: "BSC",
+    remoteId: 2,
+    chainName: "Binance Smart Chain Testnet",
+    chainId: 97,
+    rpcUrls: ["https://data-seed-prebsc-1-s1.binance.org:8545"],
+    nativeCurrency: {
+      name: "BINANCE COIN",
+      symbol: "tBNB",
+      decimals: 18,
     },
-    {
-        name: "Telos EVM Testnet",
-        remoteId: 3,
-        chainName: "Telos Testnet",
-        chainId: 41,
-        rpcUrls: ["https://testnet.telos.net/evm"],
-        nativeCurrency: {
-            name: "TLOS",
-            symbol: "TLOS",
-            decimals: 18,
-        },
-        blockExplorerUrls: ["https://testnet.telos.net/v2/explore/evm/"],
-        vaultContract: "",
+    blockExplorerUrls: ["https://testnet.bscscan.com"],
+    vaultContract: "0x5213fcB1Cf69B029A32F9454fD29f2a500af569a",
+  },
+  {
+    name: "Telos EVM",
+    remoteId: 3,
+    chainName: "Telos Testnet",
+    chainId: 41,
+    rpcUrls: ["https://testnet.telos.net/evm"],
+    nativeCurrency: {
+      name: "TLOS",
+      symbol: "TLOS",
+      decimals: 18,
     },
+    blockExplorerUrls: ["https://testnet.telos.net/v2/explore/evm/"],
+    vaultContract: "",
+  },
 ];
 
 export default async ({ store }) => {
-    const networkList = process.env.TESTNET == "true" ? testnetList : mainnetList;
-    store.commit("tport/setNetworkList", { networkList });
-    store.commit("xchain/setNetworkList", { networkList });
+  const networkList = process.env.TESTNET == "true" ? testnetList : mainnetList;
+  store.commit("tport/setNetworkList", { networkList });
+  store.commit("xchain/setNetworkList", { networkList });
 };
