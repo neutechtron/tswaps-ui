82627c22a7ed11f29595cf29c7e377700c5879ff
Th3Doo <scottiec4rock@gmail.com>
2022-06-20 12:26:25 +0200

    chore: 'Download' button on qdialog closes popup

--- src/components/bridge/NoMetaMaskDialog.vue
+++ src/components/bridge/NoMetaMaskDialog.vue
@@ -12,7 +12,7 @@
         No MetaMask Detected
       </q-card-section>
       <q-card-actions align="center">
-        <q-btn class="hover-accent" href="https://metamask.io/" target="_blank" label="Download" color="primary"/>
+        <q-btn class="hover-accent" href="https://metamask.io/" target="_blank" label="Download" color="primary" v-close-popup/>
         <q-btn class="hover-accent" label="Cancel" color="primary" @click="$emit('changeShowNoMeta')" v-close-popup/>
       </q-card-actions>
     </q-card>
