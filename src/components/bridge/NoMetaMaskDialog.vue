1b256e4ab7ec21fe27aa60fcf81fa98474ccf901
Th3Doo <scottiec4rock@gmail.com>
2022-06-20 12:25:04 +0200

    chore: qdialog 'close' button changed to read 'cancel'

--- src/components/bridge/NoMetaMaskDialog.vue
+++ src/components/bridge/NoMetaMaskDialog.vue
@@ -13,7 +13,7 @@
       </q-card-section>
       <q-card-actions align="center">
         <q-btn class="hover-accent" href="https://metamask.io/" target="_blank" label="Download" color="primary"/>
-        <q-btn class="hover-accent" label="Close" color="primary" @click="$emit('changeShowNoMeta')" v-close-popup/>
+        <q-btn class="hover-accent" label="Cancel" color="primary" @click="$emit('changeShowNoMeta')" v-close-popup/>
       </q-card-actions>
     </q-card>
   </q-dialog>
